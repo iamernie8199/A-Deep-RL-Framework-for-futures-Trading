a9a0ad623c40f87e9db78ed39aa7b0c45d313dc4
iamernie8199 <iamernie8199@gmail.com>
2021-05-26 19:06:01 +0800

    Update simple.py

--- simple.py
+++ simple.py
@@ -7,7 +7,7 @@ from stable_baselines3 import PPO
 from stable_baselines3.common.callbacks import EvalCallback
 from stable_baselines3.common.env_checker import check_env
 
-from env.env_long import TradingEnvLong
+from env.env_long2 import TradingEnvLong
 from utils import Futures
 
 warnings.filterwarnings('ignore')
@@ -47,10 +47,10 @@ eval_callback = EvalCallback(env_train, best_model_save_path='./logs/',
 QRDQN
 """
 policy_kwargs = dict(n_quantiles=50)
-model_qrdqn = QRDQN("MlpPolicy", env_train, policy_kwargs=policy_kwargs, gamma=0.9, batch_size=4096,
+"""model_qrdqn = QRDQN("MlpPolicy", env_train, policy_kwargs=policy_kwargs, gamma=0.9, batch_size=4096,
                     learning_starts=10000, buffer_size=2000000, exploration_initial_eps=0.1,
-                    optimize_memory_usage=True, device='cuda', tensorboard_log="./trading_2_tensorboard/")
-# model_qrdqn = QRDQN.load("./logs/best_model", env_train, tensorboard_log="./trading_2_tensorboard/", device='cuda', gamma=0.9)
+                    optimize_memory_usage=True, device='cuda', tensorboard_log="./trading_2_tensorboard/")"""
+model_qrdqn = QRDQN.load("./logs/best_model", env_train, tensorboard_log="./trading_2_tensorboard/", device='cuda', gamma=0.9)
 # %%
 model_qrdqn.learn(total_timesteps=10000, tb_log_name="run_DQN", callback=eval_callback, n_eval_episodes=3)
 # %%
@@ -61,19 +61,15 @@ PPO
 model_ppo = PPO.load("./logs/ppo_best_model", env=env_train, tensorboard_log="./trading_2_tensorboard/", device='cuda',
                      gamma=0.8, batch_size=1024)
 # %%
-# evaluate(model_ppo, num_episodes=5)
-# %%
 model_ppo.learn(total_timesteps=10000, tb_log_name="run_ppo", callback=eval_callback)
 # model_ppo.save("ppo")
-# model_ppo = PPO.load("ppo", env=env_train, tensorboard_log="./trading_2_tensorboard/", device='cuda', gamma=0.8)
-"""
-%load_ext tensorboard
-%tensorboard --logdir ./trading_2_tensorboard/run_DQN_19/
-"""
+
 # %%
 model = PPO.load("./logs/ppo_best_model", env=env_train, tensorboard_log="./trading_2_tensorboard/", device='cuda',
                  gamma=0.8)
-e_test_gym = TradingEnvLong(df=data_df[data_df.Date >= '2010-01-01'], futures=txf, log=True, **env_kwargs)
+#model = QRDQN.load("./logs/best_model", env_train, tensorboard_log="./trading_2_tensorboard/", device='cuda', gamma=0.9)
+
+e_test_gym = TradingEnvLong(df=data_df[data_df.Date >= '2000-01-01'], futures=txf, log=True, **env_kwargs)
 env_test, _ = e_test_gym.get_sb_env()
 # %%
 obs_test = env_test.reset()
@@ -81,4 +77,9 @@ done = False
 while not done:
     action, _states = model.predict(obs_test)
     obs_test, rewards, done, _ = env_test.step(action)
-    # env_test.render()
+    env_test.render()
+
+"""
+%load_ext tensorboard
+%tensorboard --logdir ./trading_2_tensorboard/run_DQN_19/
+"""
