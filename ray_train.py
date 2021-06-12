170437cdf2ad204f2c9d46b7641d950b1d254811
iamernie8199 <iamernie8199@gmail.com>
2021-06-12 13:58:35 +0800

    Update ray_train.py

--- ray_train.py
+++ ray_train.py
@@ -3,7 +3,7 @@ import warnings
 import pandas as pd
 from ray import tune
 from ray.tune.registry import register_env
-import ray.rllib.agents.ppo as ppo
+import ray.rllib.agents.pg as pg
 
 from env.env_long2 import TradingEnvLong
 
@@ -11,7 +11,7 @@ warnings.filterwarnings('ignore')
 
 
 def create_env(env_kwargs={}):
-    data_df = pd.read_csv("data_simple.csv")
+    data_df = pd.read_csv("D:\Documents\GitHub\A-Deep-RL-Framework-for-Index-futures-Trading\data_simple2.csv")
     data_df['Date'] = pd.to_datetime(data_df['Date'])
     train = data_df[(data_df.Date >= '2010-01-01') & (data_df.Date < '2020-01-01')]
     # the index needs to start from 0
@@ -22,7 +22,7 @@ def create_env(env_kwargs={}):
 
 register_env("TradingEnv", create_env)
 analysis = tune.run(
-    "PPO",
+    "PG",
     stop={
         "episode_reward_mean": 500
     },
@@ -32,28 +32,15 @@ analysis = tune.run(
         "log_level": "WARN",
         "framework": "tf",
         "ignore_worker_failures": True,
-        "num_workers": 2,
+        "num_workers": 0,
         "num_cpus_per_worker": 2,
         "num_gpus": 1,
-        "clip_rewards": True,
         "lr": 8e-6,
-        "lr_schedule": [
-            [0, 1e-1],
-            [int(1e2), 1e-2],
-            [int(1e3), 1e-3],
-            [int(1e4), 1e-4],
-            [int(1e5), 1e-5],
-            [int(1e6), 1e-6],
-            [int(1e7), 1e-7]
-        ],
         "gamma": 0,
-        "observation_filter": "MeanStdFilter",
-        "lambda": 0.72,
-        "vf_loss_coeff": 10,
-        "entropy_coeff": 0.01,
+        "observation_filter": "NoFilter",
     },
     # restore from the last checkpoint
-    restore=checkpoint_path,
+    #restore=checkpoint_path,
     reuse_actors=True,
     checkpoint_freq=3,
     checkpoint_at_end=True
@@ -66,32 +53,22 @@ checkpoints = analysis.get_trial_checkpoints_paths(
 )
 checkpoint_path = checkpoints[0][0]
 print(checkpoint_path)
-# C:\Users\iamer\ray_results\PPO\PPO_TradingEnv_c4d50_00000_0_2021-06-10_22-33-05\checkpoint_000005\checkpoint-5
+# C:\\Users\\iamer\\ray_results\\PPO\\PPO_TradingEnv_d524f_00000_0_2021-06-10_22-47-52\\checkpoint_000006\\checkpoint-6
+# C:\Users\iamer\ray_results\PG\PG_TradingEnv_a689c_00000_0_2021-06-11_17-08-56\checkpoint_000003\checkpoint-3
 # Restore agent
-agent = ppo.PPOTrainer(
+agent = pg.PGTrainer(
     env="TradingEnv",
     config={
-        "framework": "tf",
+        "env": "TradingEnv",
         "log_level": "WARN",
+        "framework": "tf",
         "ignore_worker_failures": True,
-        "num_workers": 2,
+        "num_workers": 0,
+        "num_cpus_per_worker": 2,
         "num_gpus": 1,
-        "clip_rewards": True,
         "lr": 8e-6,
-        "lr_schedule": [
-            [0, 1e-1],
-            [int(1e2), 1e-2],
-            [int(1e3), 1e-3],
-            [int(1e4), 1e-4],
-            [int(1e5), 1e-5],
-            [int(1e6), 1e-6],
-            [int(1e7), 1e-7]
-        ],
         "gamma": 0,
-        "observation_filter": "MeanStdFilter",
-        "lambda": 0.72,
-        "vf_loss_coeff": 10,
-        "entropy_coeff": 0.01
+        "observation_filter": "NoFilter",
     }
 )
 agent.restore(checkpoint_path)
