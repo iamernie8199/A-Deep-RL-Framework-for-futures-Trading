110cf496d724a16ff87e174e397e8589f9bf524e
iamernie8199 <iamernie8199@gmail.com>
2021-06-14 19:47:17 +0800

    Update ray_resault.py

--- ray_resault.py
+++ ray_resault.py
@@ -3,7 +3,7 @@ import warnings
 import pandas as pd
 import ray
 from ray.tune.registry import register_env
-import ray.rllib.agents.pg as pg
+import ray.rllib.agents.dqn as dqn
 
 from env.env_long2 import TradingEnvLong
 
@@ -11,41 +11,61 @@ warnings.filterwarnings('ignore')
 
 
 def create_env(env_kwargs={}):
-    data_df = pd.read_csv("D:\Documents\GitHub\A-Deep-RL-Framework-for-Index-futures-Trading\data_simple2.csv")
+    data_df = pd.read_csv("/home/sean/Docs/GitHub/A-Deep-RL-Framework-for-Index-futures-Trading/data_simple2.csv")
     data_df['Date'] = pd.to_datetime(data_df['Date'])
     train = data_df[(data_df.Date >= '2000-01-01') & (data_df.Date < '2020-01-01')]
     # the index needs to start from 0
     train = train.reset_index(drop=True)
-    env = TradingEnvLong(df=train, **env_kwargs)
+    env = TradingEnvLong(df=train, log=True, **env_kwargs)
     return env
 
 
 register_env("TestEnv", create_env)
 ray.init()
-checkpoint_path = "C:\\Users\\iamer\\ray_results\\PG\\PG_TradingEnv_a689c_00000_0_2021-06-11_17-08-56\\checkpoint_000003\\checkpoint-3"
+checkpoint_path = '/home/sean/ray_results/DQN_TestEnv_2021-06-14_14-39-40j684n7c5/checkpoint_000910/checkpoint-910'
+
 # Restore agent
-agent = pg.PGTrainer(
+agent = dqn.DQNTrainer(
     env="TestEnv",
     config={
         "env": "TradingEnv",
         "log_level": "WARN",
         "framework": "tf",
         "ignore_worker_failures": True,
-        "num_workers": 0,
-        "num_cpus_per_worker": 2,
         "num_gpus": 1,
-        "lr": 8e-6,
-        "gamma": 0,
-        "observation_filter": "NoFilter",
+        "num_atoms": 51,
+        "noisy": True,
+        "v_min": -10000.0,
+        "v_max": 10000.0,
+        "gamma": 0.9,
+        "lr": .0001,
+        "hiddens": [512],
+        "learning_starts": 10000,
+        "buffer_size": 50000,
+        "rollout_fragment_length": 4,
+        "train_batch_size": 32,
+        "exploration_config": {
+            "epsilon_timesteps": 2,
+            "final_epsilon": 0.0,
+        },
+        "target_network_update_freq": 500,
+        "prioritized_replay": True,
+        "prioritized_replay_alpha": 0.5,
+        "final_prioritized_replay_beta": 1.0,
+        "prioritized_replay_beta_annealing_timesteps": 400000,
+        "n_step": 3
     }
 )
 agent.restore(checkpoint_path)
 
 test_gym = create_env()
+out = []
+#%%
 done = False
 obs = test_gym.reset()
 while not done:
     action = agent.compute_action(obs)
-    obs, reward, done, _ = test_gym.step(action)
+    obs, reward, done, tmp = test_gym.step(action)
     test_gym.render()
+    out.append(tmp)
 
