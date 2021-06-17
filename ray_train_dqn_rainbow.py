adf11991a513d60c766290d1ced89bd160606c56
iamernie8199 <iamernie8199@gmail.com>
2021-06-17 19:35:52 +0800

    Update ray_train_dqn_rainbow.py

--- ray_train_dqn_rainbow.py
+++ ray_train_dqn_rainbow.py
@@ -13,7 +13,7 @@ warnings.filterwarnings('ignore')
 def create_env(env_kwargs={}):
     data_df = pd.read_csv("/home/sean/Docs/GitHub/A-Deep-RL-Framework-for-Index-futures-Trading/data_simple2.csv")
     data_df['Date'] = pd.to_datetime(data_df['Date'])
-    train = data_df[(data_df.Date >= '2007-01-01') & (data_df.Date < '2020-01-01')]
+    train = data_df[(data_df.Date >= '2010-01-01') & (data_df.Date < '2020-01-01')]
     # the index needs to start from 0
     train = train.reset_index(drop=True)
     env = TradingEnvLong(df=train, **env_kwargs)
