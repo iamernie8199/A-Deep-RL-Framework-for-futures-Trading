0d6526f2085df617d87cde3b27639de0b7cff919
iamernie8199 <iamernie8199@gmail.com>
2021-06-24 00:02:33 +0800

    Update sb3_resault.py

--- sb3_resault.py
+++ sb3_resault.py
@@ -17,30 +17,17 @@ env_kwargs = {}
 e_test_gym = TradingEnvLong(df=data_df[data_df.Date >= '2000-01-01'], log=True, **env_kwargs)
 """
 # %%
-# random & bnh
-out = []
-for _ in range(20):
-    info = random_rollout(e_test_gym)
-    out.append(info[0])
-result(title='random')
-out_df = pd.DataFrame(out, columns=['Net Pnl', 'rtn_on_MDD', 'PF', 'CAGR', 'num', 'winning_rate'])
-# %%
-for i in range(len(out)):
-    print(f"{i} & {int(out[i][0])} & {out[i][1]} & {out[i][2]} & {out[i][3]}\% & {out[i][4]} & {out[i][5]}\% \\")
 year_num = year_frac(e_test_gym.equity_memory['date'].iloc[0],
                      e_test_gym.equity_memory[e_test_gym.equity_memory.equity_tmp > 0]['date'].iloc[-1])
 cagr = ((out_df['Net Pnl'].mean() + e_test_gym.init_equity) / e_test_gym.init_equity) ** (1 / year_num) - 1
 print(round(cagr * 100, 3))
-# %%
-shutil.rmtree('results')
-shutil.rmtree('results_pic')
 """
 # %%
 model = DQN.load("./logs/dqn_best_model", env=e_test_gym, tensorboard_log="./trading_2_tensorboard/", device='cuda',
                     gamma=0.9, batch_size=4096, optimize_memory_usage=True,target_update_interval=5000)
 # model = PPO.load("./logs/ppo_best_model", env=e_test_gym, tensorboard_log="./trading_2_tensorboard/", device='cuda', gamma=0.8)
 # model = QRDQN.load("./logs/qrdqn_best_model", env=e_test_gym, tensorboard_log="./trading_2_tensorboard/", device='cuda', gamma=0.9)
-"""
+
 out = []
 # %%
 for _ in range(1):
@@ -52,4 +39,3 @@ for _ in range(1):
         # env_test.render()
     out.append(tmp)
 out_df = pd.DataFrame(out, columns=['Net Pnl', 'rtn_on_MDD', 'PF', 'CAGR', 'num', 'winning_rate'])
-"""
