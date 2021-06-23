259e5338c26316615284e38a7204f99244b5813c
iamernie8199 <iamernie8199@gmail.com>
2021-06-24 02:48:22 +0800

    Update thesis result.py

--- thesis result.py
+++ thesis result.py
@@ -315,3 +315,49 @@ latexsummary(out)
 split_print()
 shutil.move("results_pic", "results/Noisy-DQN")
 
+# Rainbow-DQN
+checkpoint_path = 'DQN_TestEnv_2021-06-14_19-46-29sztcqos3/checkpoint_001410/checkpoint-1410'
+agent = dqn.DQNTrainer(
+    env="TestEnv",
+    config={
+        "env": "TradingEnv",
+        "log_level": "WARN",
+        "framework": "tf",
+        "ignore_worker_failures": True,
+        "num_gpus": 1,
+        "num_atoms": 51,
+        "noisy": True,
+        "v_min": -10000.0,
+        "v_max": 10000.0,
+        "gamma": 0.9,
+        "lr": .0001,
+        "hiddens": [512],
+        "learning_starts": 10000,
+        "buffer_size": 50000,
+        "rollout_fragment_length": 4,
+        "train_batch_size": 32,
+        "exploration_config": {
+            "epsilon_timesteps": 2,
+            "final_epsilon": 0.0,
+        },
+        "target_network_update_freq": 500,
+        "prioritized_replay": True,
+        "prioritized_replay_alpha": 0.5,
+        "final_prioritized_replay_beta": 1.0,
+        "prioritized_replay_beta_annealing_timesteps": 400000,
+        "n_step": 3
+    }
+)
+agent.restore(checkpoint_path)
+out = []
+for _ in range(10):
+    done = False
+    obs = test_gym.reset()
+    while not done:
+        action = agent.compute_action(obs)
+        obs, reward, done, tmp = test_gym.step(action)
+        # test_gym.render()
+    out.append(tmp)
+latexsummary(out)
+split_print()
+shutil.move("results_pic", "results/Rainbow")
