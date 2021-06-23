52f5d665ca159f4c50f706ae1211d60bdf01ae2c
iamernie8199 <iamernie8199@gmail.com>
2021-06-24 01:57:31 +0800

    Update thesis result.py

--- thesis result.py
+++ thesis result.py
@@ -189,7 +189,7 @@ for _ in range(10):
     while not done:
         action = agent.compute_action(obs)
         obs, reward, done, tmp = test_gym.step(action)
-        #test_gym.render()
+        # test_gym.render()
     out.append(tmp)
 result_plt(title='dqn_double')
 latexsummary(out)
@@ -239,9 +239,46 @@ for _ in range(10):
     while not done:
         action = agent.compute_action(obs)
         obs, reward, done, tmp = test_gym.step(action)
-        #test_gym.render()
+        # test_gym.render()
     out.append(tmp)
 result_plt(title='dqn_duel')
 latexsummary(out)
 split_print()
 shutil.move("results_pic", "results/Dueling-DQN")
+
+# Noisy-DQN
+checkpoint_path = 'DQN_TestEnv_2021-06-18_15-33-25_c944tj8/checkpoint_000500/checkpoint-500'
+agent = dqn.DQNTrainer(
+    env="TestEnv",
+    config={
+        "env": "TradingEnv",
+        "log_level": "WARN",
+        "framework": "tf",
+        "ignore_worker_failures": True,
+        "num_gpus": 1,
+        "num_atoms": 1,
+        "noisy": True,
+        "v_min": -10000.0,
+        "v_max": 10000.0,
+        "gamma": 0.9,
+        "lr": .0001,
+        "dueling": False,
+        "hiddens": [512],
+        "learning_starts": 10000,
+        "buffer_size": 50000,
+        "rollout_fragment_length": 4,
+        "train_batch_size": 32,
+        "double_q": False,
+        "exploration_config": {
+            "epsilon_timesteps": 2,
+            "final_epsilon": 0.0,
+        },
+        "target_network_update_freq": 500,
+        "prioritized_replay": False,
+        "prioritized_replay_alpha": 0.5,
+        "final_prioritized_replay_beta": 1.0,
+        "prioritized_replay_beta_annealing_timesteps": 400000,
+        "n_step": 1
+    }
+)
+agent.restore(checkpoint_path)
