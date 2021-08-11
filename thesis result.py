8c4fe32659541391f8fcd0bf2df0657088f84d27
iamernie8199 <iamernie8199@gmail.com>
2021-08-11 21:06:59 +0800

    change path name

--- thesis result.py
+++ thesis result.py
@@ -370,7 +370,7 @@ if __name__ == "__main__":
     shutil.move("results_pic", "results/Rainbow")
 
     # PPO
-    checkpoint_path = 'PPO_TestEnv_2021-06-28_21-24-141rfn176u/checkpoint_000700/checkpoint-700'
+    checkpoint_path = 'PPO/checkpoint_000700/checkpoint-700'
     agent = ppo.PPOTrainer(
         env="TestEnv",
         config={
