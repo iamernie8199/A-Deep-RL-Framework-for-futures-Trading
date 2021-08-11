780822f0d297203f0827b5a0346bf7ffce560686
iamernie8199 <iamernie8199@gmail.com>
2021-08-11 21:58:33 +0800

    Update ray_train_dqn_eth.py

--- ray_train_dqn_eth.py
+++ ray_train_dqn_eth.py
@@ -23,9 +23,8 @@ def create_env(env_kwargs={}):
 register_env("trainEnv", create_env)
 ray.init()
 # Restore agent
-checkpoint_path = 'DQN_trainEnv_2021-06-16_03-06-12lbhm2aw3/checkpoint_004500/checkpoint-4500'
-# '/home/sean/ray_results/DQN_trainEnv_2021-06-16_03-06-12lbhm2aw3/checkpoint_004500/checkpoint-4500'
-# '/home/sean/ray_results/DQN_trainEnv_2021-06-15_21-23-183tf_lf8i/checkpoint_001500/checkpoint-1500'
+checkpoint_path = 'model/discard/rainbow_eth/checkpoint_004500/checkpoint-4500'
+# '/home/sean/ray_results/rainbow_eth/checkpoint_004500/checkpoint-4500'
 
 agent = dqn.DQNTrainer(
     env="trainEnv",
